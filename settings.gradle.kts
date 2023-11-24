a1aa59467e7801208310ec3b37fa4f1762a149e2
robisoncrisbrito <robison.brito@gmail.com>
2023-11-23 21:05:44 -0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "UsandoPreferences-PM25S-2023-2"
+include(":app")
